fcb35f27f8f14885df842ebd11f621fbe200e6b7
503 <503@503-05.academicos.cesde.edu.co>
2023-08-28 15:37:14 -0500

    Array

---
+++ src/arraytema/ArrayLista.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraytema;
+
+/**
+ *
+ * @author 503
+ */
+public class ArrayLista {
+    public static void main(String[] args) {
+        
+        
+        int [] edades  = {23,34,45,32,12,14};
+        System.out.println("la edad de la posicion 4 es:" + edades [4]);
+        
+        // para conocer el tamaño del arreglo usamos el atributo array.length
+        System.out.println("imprimame el tamaño del arreglo es:" + edades.length);
+        
+        // Podemos modificar los contenidos de la array usando la siguiente fomar
+        edades[5]= Integer.parseInt  ("18");
+        System.out.println("La posicion 4 es:" + edades [4]);
+        
+        for(int i=0; i< edades.length; i++){
+           
+    
+    
+    
+    }
+        
+        
+        
+    }
+    
+}
---
+++ src/siclofor/SicloFor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package siclofor;
+
+/**
+ *
+ * @author 503
+ */
+public class SicloFor {
+    public static void main(String[] args) {
+          
+for(int i=0; i< 8 ;i+=2)
+{//Notemos que escribir i+=2 es similar a escribir i = i + 2
+    System.out.println(i);
+}
+    }
+    
+}
